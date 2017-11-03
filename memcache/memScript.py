c3660213e34608be44c9f3052d4e3fdac9d5e8bf
maqidi <weiandedidi@163.com>
2017-11-03 18:07:33 +0800

    memche

---
+++ memcache/memScript.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import json
+import pymemcache
+
+# 链接memmcahe的client
+from pymemcache.client import Client
+
+
+# client = Client(('10.16.56.251', 11211))
+# client.set('some_key', 'some_value')
+# result = client.get('some_key')
+
+
+def add_data(value):
+    bbb = json.dumps(value)
+    return bbb
+
+
+if __name__ == '__main__':
+    key = 'xxxx.xxx'
+    value = '{ "left": [ { "t": "呆萌小狮标致307三厢舒适版 售价2万起", "turl": "//2sc.sohu.com/auto1-307sx/?pvid=264c5e4f", "p": "//2sc.sohu.com/auto1-307sx/?pvid=264c5e4f", "purl": "//5b0988e595225.cdn.sohucs.com/images/20171101/836b112a423d488580ef6075d2fb6634.png" }, { "t": "全地形SUV路虎第四代发现 售价43.6W", "turl": "//2sc.sohu.com/sccarnew/openChejiandingReport.shtml?detailReport=https://www.chejianding.com/o/partner/sohuBuy/U0FMQU4yRjQ5QkE1NjMyMjY=?trimm=%25E8%25B7%25AF%25E8%2599%258E%25E7%25AC%25AC%25E5%259B%259B%25E4%25BB%25A3%25E5%258F%2591%25E7%258E%25B02011%25E6%25AC%25BE3.0L%2520SD%2520HSE%2520%25E6%259F%25B4%25E6%25B2%25B9%25E7%2589%2588&pvid=bea7161b/?pvid=264c5e4f", "p": "//2sc.sohu.com/sccarnew/openChejiandingReport.shtml?detailReport=https://www.chejianding.com/o/partner/sohuBuy/U0FMQU4yRjQ5QkE1NjMyMjY=?trimm=%25E8%25B7%25AF%25E8%2599%258E%25E7%25AC%25AC%25E5%259B%259B%25E4%25BB%25A3%25E5%258F%2591%25E7%258E%25B02011%25E6%25AC%25BE3.0L%2520SD%2520HSE%2520%25E6%259F%25B4%25E6%25B2%25B9%25E7%2589%2588&pvid=bea7161b/?pvid=264c5e4f", "purl": "//5b0988e595225.cdn.sohucs.com/images/20171031/53c07267c5044bef82b0711e20b60dea.jpeg" }, { "t": "大众最具动感的双门轿跑车 尚酷仅售13.68万", "turl": "//2sc.sohu.com/auto1-scirocco1/?pvid=264c5e4f", "p": "//2sc.sohu.com/auto1-scirocco1/?pvid=264c5e4f", "purl": "//5b0988e595225.cdn.sohucs.com/images/20171031/f6ca6e5346e143b586023e6b434737cf.jpeg" } ], "right1": [ { "t": "准新车", "turl": "//2sc.sohu.com/buycar/a0b0c0d0e0f0g4h0j0k0m0n0/?pvid=49334f21" }, { "t": "大众", "turl": "//2sc.sohu.com/auto-volkswagen/a0b0c0d0e0f0g0h0j0k0m0n0/?pvid=49334f21" }, { "t": "帮我卖车", "turl": "//2sc.sohu.com/sellCarWeb/sellPerCar/?pvid=49334f21" }, { "t": "白菜价买好车", "turl": "//2sc.sohu.com/buycar/a0b0_10c0d0e0f0g0h0j0k0m0n0/?pvid=49334f21" }, { "t": "商家发车", "turl": "//2sc.sohu.com/ctb/?pvid=49334f21" }, { "t": "我要估价", "turl": "//2sc.sohu.com/evaluate/evaluateIndex/?pvid=49334f21" }, { "t": "MPV", "turl": "//2sc.sohu.com/buycar/a7b0c0d0e0f0g0h0j0k0m0n0/?pvid=49334f21" }, { "t": "怎样卖车最划算", "turl": "//2sc.sohu.com/sellCarWeb/sellPerCar/?pvid=49334f21" }, { "t": "5年的车能卖多少钱", "turl": "//2sc.sohu.com/evaluate/evaluateIndex/?pvid=49334f21" }, { "t": "商家发车", "turl": "//2sc.sohu.com/ctb/?pvid=49334f21" }, { "t": "本地热门车", "turl": "//2sc.sohu.com/buycar/?pvid=49334f21" } ] }'
+    json = add_data(value)
+    # value = [{'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}]
+    # json = add_data(value)
+    print json
