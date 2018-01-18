ea3a6f198fe07e9c1a9ff0f1067b30b52962fb2b
weiandedidi <weiandeidid@163.com>
2018-01-18 17:48:51 +0800

    fix

--- spider/https/get.py
+++ spider/https/get.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+# 获取二手车之家的车型库
 import urllib
 import urllib2
-
+import re
 
 values = {}
 values['username'] = "1016903103@qq.com"
 values['password'] = "XXXX"
 data = urllib.urlencode(values)
-url = "https://passport.csdn.net/account/login"
-# url = "https://www.sohu.com"
+url = "https://www.che168.com/Handler/ScriptCarList_V1.ashx?needData=1"
 geturl = url + "?" + data
-request = urllib2.Request(geturl)
+request = urllib2.Request(url)
 response = urllib2.urlopen(request)
-print response.read()
-print geturl
+# 转码
+context = re.search('charset=\S*', response.headers['Content-Type'])
+charset = context.group(0)
+charset = charset.replace("charset=", '')
+# 转码就行
+print response.read().decode(charset).encode("utf-8")
