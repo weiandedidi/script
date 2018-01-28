1145455c8a86a4f261617c9e7ec85f4ee5fa13ff
weiandedidi <weiandedidi@163.com>
2018-01-28 09:13:53 +0800

    fix

--- spider/selenium/demo_chrome.py
+++ spider/selenium/demo_chrome.py
@@ -14,6 +14,7 @@
 # 使用Selenium打开chrome浏览器
 from selenium import webdriver
 
-# 加载chrome浏览器驱动
-browser = webdriver.Chrome('D:\Program Files\chromedriver\chromedriver.exe')
+# 加载chrome浏览器驱动 webdriver.Chrome(chromedriver path)
+# browser = webdriver.Chrome('D:\Program Files\chromedriver\chromedriver.exe')
+browser = webdriver.Chrome('/Users/qidima/Work/software/chromedriver')
 browser.get('http://www.baidu.com/')
