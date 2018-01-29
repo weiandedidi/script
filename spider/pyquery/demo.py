697b0f4f426dfff4b6c7ce8f8f944802160d5bda
weiandedidi <weiandeidid@163.com>
2018-01-29 18:02:11 +0800

    update

---
+++ spider/pyquery/demo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+""" 
+@version: v1.0 
+@author: qidima 
+@license: Apache Licence  
+@contact: weiandedidi@qq.com 
+@site: https://github.com/weiandedidi/script 
+@software: PyCharm 
+@file: demo.py 
+@time: 2018/1/29 11:07 
+"""
+# pyquery的demo练习，类似jquery
+from pyquery import PyQuery as pq
+doc = pq(filename='hello.html')
+print type(doc)
+print doc.html()
+print '==========================================================='
+li = doc('li')
+print type(li)
+print li.text()
---
+++ usedcar/usedcar_site.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+""" 
+@version: v1.0 
+@author: qidima 
+@license: Apache Licence  
+@contact: weiandedidi@qq.com 
+@site: https://github.com/weiandedidi/script 
+@software: PyCharm 
+@file: usedcar_site.py 
+@time: 2018/1/29 17:50 
+"""
+# 爬取二手车之家wap网站
+
